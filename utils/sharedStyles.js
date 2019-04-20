f23ee8fa8c4cf00ca47cea32a836bb38d2223ae8
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2019-04-20 16:50:30 -0300

    Common css styles added to a helper file

---
+++ utils/sharedStyles.js
@@ -0,0 +1,33 @@
+export const buttons = {
+  buttonContainer: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    alignSelf: 'stretch',
+    marginRight: 20,
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  button: {
+    borderRadius: 5,
+    fontSize: 20,
+    padding: 20,
+    textAlign: 'center',
+  },
+  primary: {
+    backgroundColor: '#333',
+    color: '#FFF',
+  },
+  disabled: {
+    backgroundColor: '#CCC',
+    color: '#666',
+  },
+}
+
+export const containers = {
+  centeredContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+}
